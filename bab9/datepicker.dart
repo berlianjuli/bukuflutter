ec300b9372a3a70dedb75a547b19ab9802c4dd81
berlianjuli <94663421+berlianjuli@users.noreply.github.com>
2024-01-11 15:35:26 +0700

    Update datepicker.dart

--- bab9/datepicker.dart
+++ bab9/datepicker.dart
@@ -1,1 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DateTime selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text(""),
+      ),
+      body: Container(
+        child: Row(
+          children: [
+            Text(
+                "${selectedDate.day}-${selectedDate.month}-${selectedDate.year}"),
+            IconButton(
+                icon: Icon(Icons.date_range),
+                onPressed: () async {
+                  DateTime? newDate = await showDatePicker(
+                      context: context,
+                      initialDate: selectedDate,
+                      firstDate: DateTime(1970),
+                      lastDate: DateTime(2050));
+                  if (newDate != null) {
+                    setState(() {
+                      selectedDate = newDate;
+                    });
+                  }
+                })
+          ],
+        ),
+      ),
+    ));
+  }
+}
 
