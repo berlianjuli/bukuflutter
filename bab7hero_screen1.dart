f9612b926f0099504fce597a64a0cb63eaa73ec8
berlianjuli <94663421+berlianjuli@users.noreply.github.com>
2024-01-11 15:20:47 +0700

    Create bab7hero_screen1.dart

---
+++ bab7hero_screen1.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  Screen1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Screen 1")),
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: Hero(
+          tag: 'dash',
+          child: Image.asset(
+            "assets/meow.jpg",
+            fit: BoxFit.fill,
+          ),
+        ),
+      ),
+    );
+  }
+}
