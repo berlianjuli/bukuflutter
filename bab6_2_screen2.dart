46cd520c537131dd1fc8e9c41f82d2e76f77c46e
berlianjuli <94663421+berlianjuli@users.noreply.github.com>
2024-01-11 15:12:59 +0700

    Create bab6_2_screen2.dart

---
+++ bab6_2_screen2.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class screen2 extends StatefulWidget {
+  const screen2({super.key});
+
+  @override
+  State<screen2> createState() => _screen2State();
+}
+
+class _screen2State extends State<screen2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Screen 2"),
+        ),
+        body: Container(
+          child: OutlinedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Text('Back'),
+          ),
+        ),
+    );
+  }}
