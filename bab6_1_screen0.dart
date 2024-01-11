101c103559b4c578bffaea6f9b7043e683a771b6
berlianjuli <94663421+berlianjuli@users.noreply.github.com>
2024-01-11 15:08:52 +0700

    Create bab6_1_screen0.dart

---
+++ bab6_1_screen0.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screen1.dart';
+class screen0 extends StatelessWidget {
+  const screen0({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+          child: OutlinedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(
+                      builder:((context) => screen1())
+                    ));
+                  },
+                  child: const Text('Go to Screen 1'),
+                ),
+        );
+  }
+}
