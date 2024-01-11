ae62c88f067c6908fa1bda0217e3534d14a32e60
berlianjuli <94663421+berlianjuli@users.noreply.github.com>
2024-01-11 14:30:49 +0700

    Create bab4_icon_dalam_column.dart

---
+++ bab4_icon_dalam_column.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Aplikasiku"),
+        ),body: Container(
+          child: Column(
+            children: [
+              Icon(Icons.email),
+              Icon(Icons.contact_emergency),
+              Icon(Icons.access_alarm)
+            ],
+          ),
+        )
+      ),
+    );
+  }
+}
